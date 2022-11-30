9a3e154cf0a5467d2affc96a327e5c290cd333a2
amanjaiswal-cedcoss <amanjaiswal@cedcoss.com>
2022-11-30 10:02:41 +0530

    Setup two different configuration environments

--- config.dev.js
+++ config.dev.js
@@ -2,10 +2,6 @@ const path=require('path')
 module.exports={
     mode:"development",
     entry:'./src/App.js',
-    output:{
-        path:path.resolve(__dirname,"dist"),
-        filename:"output.js",   
-    },
     devServer:{
         static: {
             directory: path.join(__dirname, 'dist'),
---
+++ config.prod.js
@@ -0,0 +1,17 @@
+const path=require('path')
+module.exports={
+    mode:"development",
+    entry:'./src/App.js',
+    output:{
+        path:path.resolve(__dirname,"dist"),
+        filename:"output.js",   
+    },
+     module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+}
